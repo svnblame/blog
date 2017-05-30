4675132293daf1e3f90fbf82299a063b5eba112f
Gene Kelley <eugenek@guildmortgage.net>
2017-05-29 17:18:26 -0700

    Associate Posts with Users.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::get('/', 'PostsController@index');
+Route::get('/', 'PostsController@index')->name('home');
 Route::get('/posts/create', 'PostsController@create');
 Route::post('posts/{post}/comments', 'CommentsController@store');
 Route::post('/posts', 'PostsController@store');
@@ -8,3 +8,10 @@ Route::get('posts/{post}', 'PostsController@show');
 
 Route::get('tasks', 'TasksController@index');
 Route::get('tasks/{task}', 'TasksController@show');
+
+Route::get('register', 'RegistrationController@create');
+Route::post('register', 'RegistrationController@store');
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store');
+Route::get('logout', 'SessionsController@destroy');
