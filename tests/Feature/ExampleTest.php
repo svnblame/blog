19524ebb539dcaa1c7d73bd308f14090f125790d
Gene Kelley <eugenek@guildmortgage.net>
2017-06-13 13:25:23 -0700

    Testing 101

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,13 +2,16 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
+use App\Post;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseTransactions;
+
     /**
      * A basic test example.
      *
@@ -16,8 +19,32 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        // Given - I have 2 records in the database that are posts,
+        // and each one is posted a month apart ...
 
-        $response->assertStatus(200);
+        // Fake a Post created within this month...
+        $first = factory(Post::class)->create();
+
+        // Fake a Post created one month ago...
+        $second = factory(Post::class)->create([
+            'created_at' => \Carbon\Carbon::now()->subMonth()
+        ]);
+
+        // When - I fetch the archives ...
+        $posts = Post::archives();
+
+        // Then - the response should be in the proper format
+        $this->assertEquals([
+            [
+                "year" => $first->created_at->format('Y'),
+                "month" => $first->created_at->format('F'),
+                "published" => 1
+            ],
+            [
+                "year" => $second->created_at->format('Y'),
+                "month" => $second->created_at->format('F'),
+                "published" => 1
+            ],
+        ], $posts);
     }
 }
