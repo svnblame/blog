b079101cd5e2e17e366230374895a59d0c3cfcee
Gene Kelley <eugenek@guildmortgage.net>
2017-06-13 18:16:46 -0700

    Sending Email with Markdown

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+	</head>
+	<body>
+		<h1>Welcome to Gene's Blog Site!</h1>
+		<p>{{ $user->name }}, we would like to welcome you!</p>
+	</body>
+</html>
